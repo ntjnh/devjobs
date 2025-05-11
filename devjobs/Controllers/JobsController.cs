e917573c17d2cd77d0af7a4701cf25790371bca0
Nate <codebynate@gmail.com>
2025-05-11 22:31:06 +0100

    Add Jobs controller

---
+++ devjobs/Controllers/JobsController.cs
@@ -0,0 +1,41 @@
+using devjobs.Models;
+using devjobs.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace devjobs.Controllers;
+
+public class JobsController : Controller
+{
+    private readonly JobsService _jobsService;
+
+    public JobsController(JobsService jobsService) => _jobsService = jobsService;
+
+    [Route("Jobs")]
+    [Route("Jobs/Index")]
+    public async Task<IActionResult> Index()
+    {
+        var viewModel = new HomeViewModel
+        {
+            Jobs = await _jobsService.GetAsync()
+        };
+        return View(viewModel);
+    }
+
+    [Route("/Jobs/{id?}")]
+    public async Task<IActionResult> Job(string id)
+    {
+        var job = await _jobsService.GetAsync(id);
+
+        if (job is null)
+        {
+            return NotFound();
+        }
+
+        var viewModel = new JobViewModel
+        {
+            JobDescription = job
+        };
+
+        return View("Job", viewModel);
+    }
+}
