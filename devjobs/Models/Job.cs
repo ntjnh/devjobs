aa493bbb680d63a2403ba177533b7dca213a05f6
Nate <codebynate@gmail.com>
2025-05-15 14:20:52 +0100

    Add NotNull attributes to job fields
    Gets rid of null reference warnings - they will never be null

--- devjobs/Models/Job.cs
+++ devjobs/Models/Job.cs
@@ -1,5 +1,6 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace devjobs.Models;
@@ -9,17 +10,21 @@ public class Job
     public class Requirements
     {
         [BsonElement("content")]
+        [NotNull]
         public string? Content { get; set; }
 
         [BsonElement("items")]
+        [NotNull]
         public List<string>? Items { get; set; }
     }
     public class Role
     {
         [BsonElement("content")]
+        [NotNull]
         public string? Content { get; set; }
 
         [BsonElement("items")]
+        [NotNull]
         public List<string>? Items { get; set; }
     }
 
@@ -28,34 +33,44 @@ public class Job
     public string? Id { get; set; }
 
     [BsonElement("company")]
+    [NotNull]
     public string? Company { get; set; }
 
     [BsonElement("logo")]
+    [NotNull]
     public string? Logo { get; set; }
 
     [BsonElement("logoBackground")]
+    [NotNull]
     public string? LogoBackground { get; set; }
 
     [BsonElement("position")]
     [JsonPropertyName("position")]
+    [NotNull]
     public string? Position { get; set; }
     
     [BsonElement("postedAt")]
+    [NotNull]
     public string? PostedAt { get; set; }
     
     [BsonElement("contract")]
+    [NotNull]
     public string? Contract { get; set; }
     
     [BsonElement("location")]
+    [NotNull]
     public string? Location { get; set; }
     
     [BsonElement("website")]
+    [NotNull]
     public string? Website { get; set; }
     
     [BsonElement("apply")]
+    [NotNull]
     public string? Apply { get; set; }
     
     [BsonElement("description")]
+    [NotNull]
     public string? Description { get; set; }
     
     [BsonElement("requirements")]
