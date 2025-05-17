726544bf3a99aee565f53c8022e2df26e11b94aa
Nate <codebynate@gmail.com>
2025-05-17 09:12:45 +0100

    Update Jobs service Get declaration

--- devjobs/Services/JobsService.cs
+++ devjobs/Services/JobsService.cs
@@ -17,8 +17,9 @@ public class JobsService
         _jobsCollection = mongoDatabase.GetCollection<Job>(jobsDatabaseSettings.Value.JobsCollectionName);
     }
 
-    public async Task<List<Job>> GetAsync() => await _jobsCollection.Find(_ => true).ToListAsync();
-    public IQueryable<Job> GetQueryableAsync() => _jobsCollection.AsQueryable();
+    // public async Task<List<Job>> GetAsync() => await _jobsCollection.Find(_ => true).ToListAsync();
+    public IMongoCollection<Job> Get() => _jobsCollection;
+    // public Task<IQueryable<Job>> GetAsync() => _jobsCollection.Find(_ => true);
 
     public async Task<Job?> GetAsync(string id) => await _jobsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 }
