b3286f6475eb5a119175951cfef49701875df1c7
Nate <codebynate@gmail.com>
2025-05-23 22:43:59 +0100

    Tidy up jobs controller and use mongo builder instead of linq

--- devjobs/Controllers/JobsController.cs
+++ devjobs/Controllers/JobsController.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using devjobs.Models;
 using devjobs.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -15,65 +16,57 @@ public class JobsController : Controller
     public async Task<IActionResult> Index(
         string? keyword, 
         string? location, 
-        bool FullTime,
-        int? pageNumber)
+        bool FullTime)
     { 
-        var jobs = _jobsService.Get();
+        var jobs = _jobsService.GetJobs();
         var viewModel = new JobsViewModel();
         var builder = Builders<Job>.Filter;
-        var filter = builder.Empty;
-        var results = jobs.Find(filter);
 
-        // If keyword/location has been entered or Full Time is checked then 
-        // set page number to 1
-        if (!string.IsNullOrEmpty(keyword) ||
-            !string.IsNullOrEmpty(location) ||
-            FullTime)
+        // Filters
+        var baseFilter = builder.Empty;
+        var ftFilter = baseFilter;
+        var keywordFilter = baseFilter;
+        var locationFilter = baseFilter;
+        
+        var results = await jobs.FindAsync(baseFilter);
+
+        var keywordEntered = false;
+        var locationEntered = false;
+
+        if (FullTime)
         {
-            pageNumber = 1;
+            ftFilter = builder.Eq(j => j.Contract, "Full Time");
         }
 
         if (!string.IsNullOrEmpty(keyword))
         {
-            // jobs = jobs.Where(j => {
-            //     var checkPosition = j.Position.Contains(keyword, StringComparison.CurrentCultureIgnoreCase);
-            //     var checkCompany = j.Company.Contains(keyword, StringComparison.CurrentCultureIgnoreCase);
-
-            //     return checkPosition || checkCompany;
-
-            // }).ToList();
-            filter = builder.Eq(j => j.Position, keyword) | builder.Eq(j => j.Company, keyword);
-            results = jobs.Find(filter);
+            keywordEntered = true;
+            keywordFilter = builder.Regex(
+                j => j.Position, new Regex(keyword, RegexOptions.IgnoreCase)) |
+                builder.Regex(j => j.Company, new Regex(keyword, RegexOptions.IgnoreCase));
         }
 
         if (!string.IsNullOrEmpty(location))
         {
-            // jobs = jobs.Where(j => j.Location.ToLower().Contains(location.ToLower())).ToList();
-            filter = builder.Eq(j => j.Location.ToLower(), location.ToLower());
-            results = jobs.Find(filter);
+            locationEntered = true;
+            locationFilter = builder.Regex(
+                j => j.Location, new Regex(location, RegexOptions.IgnoreCase));
         }
 
-        if (FullTime)
+        if (FullTime || keywordEntered || locationEntered)
         {
-            // jobs = jobs.Where(j => j.Contract == "Full Time").ToList();
-            filter = builder.Eq(j => j.Contract, "Full Time");
-            results = jobs.Find(filter);
+            results = await jobs.FindAsync(ftFilter & keywordFilter & locationFilter);
         }
 
-        viewModel.Jobs = results.ToList();
-
-
-        // int pageSize = 9;
-        // var jobsQ = _jobsService.GetQueryable();
-        // var xyz = await PaginatedList<Job>.CreateAsync(jobsQ, pageNumber ?? 1, pageSize);
-
+        var searchResults = results.ToList();
+        viewModel.Jobs = searchResults;
         return View(viewModel);
     }
 
     [Route("/Jobs/{id?}")]
     public async Task<IActionResult> Job(string id)
     {
-        var job = await _jobsService.GetAsync(id);
+        var job = await _jobsService.GetJobAsync(id);
 
         if (job is null)
         {
